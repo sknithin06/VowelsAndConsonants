0157e735cefc08230b3056bda6135a036e880960
sknithin06 <nithinsk939@gmail.com>
2021-07-04 05:15:14 +0530

    Enter a Letter

--- vowelsandconsonant.java
+++ vowelsandconsonant.java
@@ -1,7 +1,22 @@
 package Java;
-
+import java.util.Scanner;
 public class vowelsandconsonant {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Alphabet");
+
+		Scanner sc= new Scanner(System.in);   
+		System.out.print("Enter a Letter:");  
+		char c = sc.next().charAt(0);
+		
+		if ( c == 'A'|| c == 'a' || 
+				c == 'e' || c == 'E' || 
+				c == 'i' || c == 'I' || 
+				c == 'o' || c == 'O' ||
+				c == 'u' || c == 'U') {
+			System.out.println("Given Alphabet is Vowel:"+c);
+		}
+		else {
+			System.out.println("Given Alphabet is Consonant:"+c);
+		}
 	}
 }
